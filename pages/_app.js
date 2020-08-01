30558663eade7b28601832ac0db589c1d51ac7d8
hatsu38 <hajiwata0308@gmail.com>
2020-08-01 19:39:17 +0900

    Add TailWindCSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../stylesheets/layout.module.css'
-
+import '../stylesheets/global.css'
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import About from '../components/about'
 export default class Home extends Component {
   render() {
     return (
@@ -12,6 +13,7 @@ export default class Home extends Component {
         <div className="container">
           <h1>hoge</h1>
           Read <Link href="/cafes"><a>to Cafes Page!</a></Link>
+          <About />
         </div>
       </Layout>
     )
