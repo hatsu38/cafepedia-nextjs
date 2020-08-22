dbf9a5a3d5daa1efe6e19592cf899dfb81d9b437
hatsu38 <hajiwata0308@gmail.com>
2020-08-22 20:22:41 +0900

    Chnage API Host to Statgin

--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ const withSass = require('@zeit/next-sass');
 
 module.exports = withSass({
   experimental: { scss: true },
+  env: {
+    apiHost: 'https://cafepedia-api-staging.herokuapp.com/api/v1/',
+    s3Host: 'https://cafepedia-images.s3-ap-northeast-1.amazonaws.com'
+  }
 })
