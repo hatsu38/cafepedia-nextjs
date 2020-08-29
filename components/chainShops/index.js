3236b29c59e7ee38ca4956cee70ca64a4ea6ef31
hatsu38 <hajiwata0308@gmail.com>
2020-08-29 18:25:08 +0900

    Add ChainShops Linkt in Chainshops Components

--- components/chainShops/index.js
+++ components/chainShops/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import PropTypes from "prop-types"
+import Link from "next/link"
 import { Row, Col, Card } from "react-bootstrap"
 
 const propTypes = {
@@ -20,16 +21,23 @@ export default class Index extends Component {
           {chainShops.slice(0, 8).map((chainShop) => (
             <Col xs={6} sm={3} lg key={chainShop.eng_name} className="mt-3">
               <Card className="h-100 mx-2 mx-lg-1">
-                <Card.Img
-                  variant="top"
-                  className={`bg-${chainShop.eng_name}`}
-                  src={process.env.s3Host + chainShop.image}
-                />
-                <Card.Body className="py-1 px-1 border-top">
-                  <Card.Title className="f7 mb-0 text-center">
-                    {chainShop.name}
-                  </Card.Title>
-                </Card.Body>
+                <Link
+                  href="/[chain_shop_eng_name]"
+                  as={`/${chainShop.eng_name}`}
+                >
+                  <a href={`/${chainShop.eng_name}`}>
+                    <Card.Img
+                      variant="top"
+                      className={`bg-${chainShop.eng_name}`}
+                      src={process.env.s3Host + chainShop.image}
+                    />
+                    <Card.Body className="py-1 px-1 border-top">
+                      <Card.Title className="f7 mb-0 text-center">
+                        {chainShop.name}
+                      </Card.Title>
+                    </Card.Body>
+                  </a>
+                </Link>
               </Card>
             </Col>
           ))}
