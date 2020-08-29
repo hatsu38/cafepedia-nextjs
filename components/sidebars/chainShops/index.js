a09208c26ca3ef666f0b7abed6dedbf0cae3e469
hatsu38 <hajiwata0308@gmail.com>
2020-08-29 17:44:40 +0900

    Create ChainShops Sidebar

--- components/sidebars/chainShops/index.js
+++ components/sidebars/chainShops/index.js
@@ -7,8 +7,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStoreAlt } from "@fortawesome/free-solid-svg-icons"
 
 const propTypes = {
-  cities: PropTypes.array.isRequired,
-  prefecture: PropTypes.object.isRequired,
+  chainShops: PropTypes.array.isRequired,
+  chainShop: PropTypes.object,
+  prefecture: PropTypes.object,
+  city: PropTypes.object,
 }
 
 export default class Index extends Component {
@@ -17,7 +19,18 @@ export default class Index extends Component {
   }
 
   render() {
-    const { cities, prefecture } = this.props
+    const { chainShops, chainShop, prefecture, city } = this.props
+
+    let componentPath = "/[chain_shop_eng_name]"
+    let prefixToLink = "/"
+
+    if (prefecture && city) {
+      componentPath = "/[prefecture_name_e]/[city_code]/[chain_shop_eng_name]"
+      prefixToLink = `/${prefecture.name_e}/${city.code}/`
+    } else if (prefecture) {
+      componentPath = "/[prefecture_name_e]/[chain_shop_eng_name]"
+      prefixToLink = `/${prefecture.eng_name}/`
+    }
 
     return (
       <section className="list-sidebar">
@@ -25,28 +38,36 @@ export default class Index extends Component {
           <FontAwesomeIcon icon={faStoreAlt} className="mr-2 accent-text" />
           チェーン店から探す
         </h2>
-        <ul className="related__links">
-          <li>
-            <Link href="/[prefecture_name_e]" as={`/${prefecture.name_e}`}>
-              <a href={`/${prefecture.name_e}`} className="related__link f8">
-                {prefecture.name}
-              </a>
-            </Link>
-          </li>
-        </ul>
-        <hr className="mt-2 mb-0" />
-        <ul className="pl-0 mb-0">
-          {cities.map((city) => (
-            <li key={city.id} className="d-inline-block mt-1 ml-1">
+        {chainShop && (
+          <ul className="related__links">
+            <li>
               <Link
-                href="/[prefecture_name_e]/[city_code]"
-                as={`/${prefecture.name_e}/${city.code}`}
+                href={componentPath}
+                as={`${prefixToLink}${chainShop.eng_name}`}
               >
                 <a
-                  href={`/${prefecture.name_e}/${city.code}`}
+                  href={`${prefixToLink}${chainShop.eng_name}`}
+                  className="related__link f8"
+                >
+                  {chainShop.name}
+                </a>
+              </Link>
+            </li>
+          </ul>
+        )}
+        <hr className="mt-2 mb-0" />
+        <ul className="pl-0 mb-0">
+          {chainShops.map((chainShop) => (
+            <li key={chainShop.eng_name} className="d-inline-block mt-1 ml-1">
+              <Link
+                href={componentPath}
+                as={`${prefixToLink}${chainShop.eng_name}`}
+              >
+                <a
+                  href={`${prefixToLink}${chainShop.eng_name}`}
                   className={"chain-shop--item f8"}
                 >
-                  {city.name}
+                  {chainShop.name}
                 </a>
               </Link>
             </li>
