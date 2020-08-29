1d4f3c2a2cb092a987467d97ea8e10ee6086e9ea
hatsu38 <hajiwata0308@gmail.com>
2020-08-29 18:11:57 +0900

    s3Host is constant

--- components/chainShops/index.js
+++ components/chainShops/index.js
@@ -17,16 +17,13 @@ export default class Index extends Component {
       <section className="mt-5">
         <h2 className="section--title">人気チェーン店から探す</h2>
         <Row noGutters>
-          {chainShops.map((chainShop) => (
-            <Col xs={6} sm={3} lg key={chainShop.id} className="mt-3">
+          {chainShops.slice(0, 8).map((chainShop) => (
+            <Col xs={6} sm={3} lg key={chainShop.eng_name} className="mt-3">
               <Card className="h-100 mx-2 mx-lg-1">
                 <Card.Img
                   variant="top"
                   className={`bg-${chainShop.eng_name}`}
-                  src={
-                    "https://cafepedia-images.s3-ap-northeast-1.amazonaws.com" +
-                    chainShop.image
-                  }
+                  src={process.env.s3Host + chainShop.image}
                 />
                 <Card.Body className="py-1 px-1 border-top">
                   <Card.Title className="f7 mb-0 text-center">
