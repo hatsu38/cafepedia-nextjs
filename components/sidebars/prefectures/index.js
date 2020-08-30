47c3f62c6aa72918e8ba939a464e54b794a8bf34
hatsu38 <hajiwata0308@gmail.com>
2020-08-30 12:26:57 +0900

    Create ChainShop Pages

---
+++ components/sidebars/prefectures/index.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import Link from "next/link"
+
+import "components/sidebars/styles/index.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMap } from "@fortawesome/free-solid-svg-icons"
+
+const propTypes = {
+  prefectures: PropTypes.array.isRequired,
+  prefecture: PropTypes.object,
+  chainShop: PropTypes.object.isRequired,
+}
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { prefectures, prefecture, chainShop } = this.props
+
+    return (
+      <section className="list-sidebar">
+        <h2 className="list-sidebar--title m-0">
+          <FontAwesomeIcon icon={faMap} className="mr-2 accent-text" />
+          都道府県から探す
+        </h2>
+        {prefecture && (
+          <ul className="related__links">
+            <li>
+              <Link href="/[prefecture_name_e]" as={`/${prefecture.name_e}`}>
+                <a href={`/${prefecture.name_e}`} className="related__link f8">
+                  {prefecture.name}
+                </a>
+              </Link>
+            </li>
+          </ul>
+        )}
+        <hr className="mt-2 mb-0" />
+        <ul className="pl-0 mb-0">
+          {prefectures.map((prefecture) => (
+            <li key={prefecture.name_e} className="d-inline-block mt-1 ml-1">
+              <Link
+                href="/[prefecture_name_e]/chain_shops/[eng_name]"
+                as={`/${prefecture.name_e}/chain_shops/${chainShop.eng_name}`}
+              >
+                <a
+                  href={`/${prefecture.name_e}/chain_shops/${chainShop.eng_name}`}
+                  className={"chain-shop--item f8"}
+                >
+                  {prefecture.name}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+  }
+}
+
+Index.propTypes = propTypes
---
+++ pages/chain_shops/[eng_name]/index.js
@@ -0,0 +1,82 @@
+import React from "react"
+import PropTypes from "prop-types"
+import Head from "next/head"
+import { useRouter } from "next/router"
+
+import fetch from "isomorphic-unfetch"
+
+import { Container } from "react-bootstrap"
+import Layout from "components/layout"
+import ShopLists from "components/shopLists"
+import Prefectures from "components/sidebars/prefectures"
+import Stations from "components/sidebars/stations"
+
+import "stylesheets/prefecture_name_e.module.scss"
+
+const propTypes = {
+  prefectures: PropTypes.object,
+  stations: PropTypes.array,
+  chainShop: PropTypes.object,
+  shops: PropTypes.array,
+}
+
+export default function Index({ prefectures, stations, chainShop, shops }) {
+  const router = useRouter()
+  if (router.isFallback) {
+    return <div>Loading...</div>
+  }
+
+  return (
+    <Layout>
+      <Head>
+        <title>
+          カフェペディア |{chainShop.name}の電源のあるカフェ{shops.length}選
+        </title>
+      </Head>
+      <Container className="d-flex">
+        <div className="sidebars-left">
+          <Stations stations={stations} />
+          <Prefectures prefectures={prefectures} chainShop={chainShop} />
+        </div>
+        <div className="main-columns ml-3">
+          <h1 className="main-columns--title">
+            {chainShop.name}の電源のあるカフェ{shops.length}選
+          </h1>
+          <ShopLists shops={shops} />
+        </div>
+      </Container>
+    </Layout>
+  )
+}
+
+Index.propTypes = propTypes
+
+export async function getStaticPaths() {
+  const res = await fetch(`${process.env.apiHost}main_shops`)
+  const json = await res.json()
+  const chainShops = json.main_shops
+
+  const paths = chainShops.map((chainShop) => ({
+    params: {
+      eng_name: chainShop.eng_name,
+    },
+  }))
+
+  return { paths, fallback: true }
+}
+
+export async function getStaticProps({ params }) {
+  const response = await fetch(
+    `${process.env.apiHost}/main_shops/${params.eng_name}`
+  )
+  const json = await response.json()
+
+  const prefectures = json.prefectures
+  const stations = json.stations
+  const chainShop = json.main_shop
+  const shops = json.shops
+
+  return {
+    props: { prefectures, stations, chainShop, shops },
+  }
+}
