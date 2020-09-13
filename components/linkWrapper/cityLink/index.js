6fdfe0a7f6c0da8bac047f3ae6cc20b425772755
hatsu38 <hajiwata0308@gmail.com>
2020-09-13 11:32:07 +0900

    Fix Proptypes

--- components/linkWrapper/cityLink/index.js
+++ components/linkWrapper/cityLink/index.js
@@ -3,9 +3,10 @@ import PropTypes from "prop-types"
 import LinkWithATag from "components/linkWrapper/linkWithATag"
 
 const propTypes = {
-  children: PropTypes.object.isRequired,
   city: PropTypes.object.isRequired,
   classes: PropTypes.string,
+  children: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
+    .isRequired,
 }
 
 export default function Layout({ city, classes, children }) {
