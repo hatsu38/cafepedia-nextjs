7cc39f85c6ecbbe35a83e854dca9b470a77206bf
hatsu38 <hajiwata0308@gmail.com>
2020-09-20 20:10:05 +0900

    Fix Footer by lint

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Navbar, Container } from "react-bootstrap"
 import { useRouter } from "next/router"
 
 export default function Header() {
@@ -14,8 +13,12 @@ export default function Header() {
   }
   return (
     <footer className="f6 bg-lighten-15-original-gray white-text mt-5">
-      <div className="text-center py-3">© 2020 Copyright:
-        <a href="https://cafepedia.jp/" className="white-text"> cafepedia.jp</a>
+      <div className="text-center py-3">
+        © 2020 Copyright:
+        <a href="https://cafepedia.jp/" className="white-text">
+          {" "}
+          cafepedia.jp
+        </a>
       </div>
     </footer>
   )
