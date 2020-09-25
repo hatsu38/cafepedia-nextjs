254c97d446a4a244b352bd340ae28fa5bf23911f
hatsu38 <hajiwata0308@gmail.com>
2020-09-25 22:07:28 +0900

    Api Host change

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const withSass = require("@zeit/next-sass")
 module.exports = withSass({
   experimental: { scss: true },
   env: {
-    apiHost: "https://cafepedia-api-staging.herokuapp.com/api/v1/",
+    apiHost: "https://cafepedia-api.herokuapp.com/api/v1/",
     s3Host: "https://cafepedia-images.s3-ap-northeast-1.amazonaws.com",
     GOOGLE_MAP_API_KEY: process.env.GOOGLE_MAP_API_KEY,
     SENTRY_DSN: process.env.SENTRY_DSN,
