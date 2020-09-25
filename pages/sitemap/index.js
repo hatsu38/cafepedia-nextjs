b011c0c8480d8729769e7d10a35a20baa4d1b882
hatsu38 <hajiwata0308@gmail.com>
2020-09-25 22:38:35 +0900

    redirect

--- pages/sitemap/index.js
+++ pages/sitemap/index.js
@@ -1,3 +1,7 @@
+import React from "react"
+export default function Index() {
+  return <div />
+}
 export async function getServerSideProps({ res }) {
   res.statusCode = 302
   res.writeHead(302, { Location: process.env.SITEMAP_URL })
