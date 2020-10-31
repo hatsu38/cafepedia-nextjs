d32f3b5a064087b2fbd67fb1ff82cb60ab3d3952
hatsu38 <hajiwata0308@gmail.com>
2020-10-31 20:12:18 +0900

    Addd popular cities

---
+++ components/popularCities/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react"
+import PropTypes from "prop-types"
+import CityLink from "components/linkWrapper/cityLink"
+import { Badge } from "react-bootstrap"
+
+import "./index.module.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons"
+
+const propTypes = {
+  cities: PropTypes.array.isRequired,
+}
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const { cities } = this.props
+
+    return (
+      <section className="mt-5">
+        <h2 className="section--title">人気の市区町村から探す</h2>
+        {cities.map((city, idx) => (
+          <CityLink city={city} key={city.id}>
+            <Badge
+              className={`station--tag original-gray mr-sm-2 mr-1 mt-sm-3 mt-2 py-1 px-2 ${
+                idx > 14 && "d-none d-sm-inline-block"
+              }`}
+            >
+              <FontAwesomeIcon
+                icon={faMapMarkerAlt}
+                className="mr-1"
+                width="14"
+                height="15"
+              />
+              {city.name}
+            </Badge>
+          </CityLink>
+        ))}
+      </section>
+    )
+  }
+}
+
+Index.propTypes = propTypes
