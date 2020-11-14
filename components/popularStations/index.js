ddd168061e9bc18f9c5d85e3e0023db229b2ab4e
hatsu38 <hajiwata0308@gmail.com>
2020-11-14 16:50:42 +0900

    popularStations Component to function component

--- components/popularStations/index.js
+++ components/popularStations/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React from "react"
 import PropTypes from "prop-types"
 import StationLink from "components/linkWrapper/stationLink"
 import { Badge } from "react-bootstrap"
@@ -11,37 +11,31 @@ const propTypes = {
   stations: PropTypes.array.isRequired,
 }
 
-export default class Index extends Component {
-  constructor(props) {
-    super(props)
-  }
+export default function Index(props) {
+  const { stations } = props
 
-  render() {
-    const { stations } = this.props
-
-    return (
-      <section className="mt-5">
-        <h2 className="section--title">人気の駅から探す</h2>
-        {stations.map((station, idx) => (
-          <StationLink station={station} key={station.id}>
-            <Badge
-              className={`station--tag original-gray mr-sm-2 mr-1 mt-sm-3 mt-2 py-1 px-2 ${
-                idx > 14 && "d-none d-sm-inline-block"
-              }`}
-            >
-              <FontAwesomeIcon
-                icon={faTrain}
-                className="mr-1"
-                width="14"
-                height="15"
-              />
-              {station.kanji_name}
-            </Badge>
-          </StationLink>
-        ))}
-      </section>
-    )
-  }
+  return (
+    <section className="mt-5">
+      <h2 className="section--title">人気の駅から探す</h2>
+      {stations.map((station, idx) => (
+        <StationLink station={station} key={station.id}>
+          <Badge
+            className={`station--tag original-gray mr-sm-2 mr-1 mt-sm-3 mt-2 py-1 px-2 ${
+              idx > 14 && "d-none d-sm-inline-block"
+            }`}
+          >
+            <FontAwesomeIcon
+              icon={faTrain}
+              className="mr-1"
+              width="14"
+              height="15"
+            />
+            {station.kanji_name}
+          </Badge>
+        </StationLink>
+      ))}
+    </section>
+  )
 }
 
 Index.propTypes = propTypes
